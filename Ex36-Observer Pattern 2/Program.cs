a6a1b9767170458d0c1fa335d9cd22f43bb3cdf4
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-12-12 08:47:29 +0100

    Add project files.

---
+++ Ex36-Observer Pattern 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex36_Observer_Pattern_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
